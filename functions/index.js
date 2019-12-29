7c94da557617c21e0f97743c0a6fe29f3e54048e
Tsai, Tsai-Chieh <jecica196@gmail.com>
2019-12-29 23:06:29 +0800

    add more bodyParser setting

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,11 @@
 const modules = require('./util/modules');
 // const functions = modules.functions;
 const app = modules.express();
-const Busboy = require('busboy');
-const multer = require('multer');
-app.use(modules.bodyParser.json());
 // create application/x-www-form-urlencoded parser
-app.use(modules.bodyParser.urlencoded({ extended: true }));
+app.use(modules.bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(modules.bodyParser.json());
 
-app.use('/products', require('./routers/products'));
-
-app.post('/test', function(req, res) {
-  console.log(req.file);
-  res.send(req.files);
-});
+app.use('/product', require('./routers/product'));
 
 exports.api = modules.functions.https.onRequest(app);
